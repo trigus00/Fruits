23f1ddfddf1dd0c91dd37d0e89ff62dfd247d3f6
SNaseerTX <34410147+ShahzadNaseer@users.noreply.github.com>
2019-11-18 23:47:45 -0600

    For Browse the Folder

---
+++ Project3_Webpage/js/app.js
@@ -0,0 +1,13 @@
+$('document').ready(function () {
+    $("#imgload").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                // console.log("reader.onload");
+                $('#imgshow').attr('src', e.target.result);
+                $('#imgtext').attr('value', e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
